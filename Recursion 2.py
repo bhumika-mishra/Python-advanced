16bd2650585a0f51ec8ac63dd8538d733397c5aa
bhumika-mishra <96329652+bhumika-mishra@users.noreply.github.com>
2025-11-23 21:40:39 +0530

    Add files via upload

--- Recursion 2.py
+++ Recursion 2.py
@@ -5,4 +5,23 @@ def headrec(n,num):
     headrec(n+1,num)
     print(n)
 n= int(input("Enter n to print 1 to n : "))    
-headrec(1,n)
+#headrec(1,n)
+
+#Tail Recursion
+def tailrec(n,num):
+    if n>num:
+        return
+    print(n)
+    tailrec(n+1,num)
+n= int(input("Enter n to print 1 to n : "))    
+#tailrec(1,n)
+
+#Tree recursion
+def incdec(n,num):
+    if(n<1 or n>num):
+        return
+    print(n)
+    incdec(n-1,num)
+    print(n)
+n= int(input("Enter n to print 1 to n : "))    
+incdec(n,n)    
