0d930908d757325fa1d66e791b44e2bc7cd33022
bhumika-mishra <96329652+bhumika-mishra@users.noreply.github.com>
2025-12-18 18:29:19 +0530

    Add files via upload

---
+++ Array leader.py
@@ -0,0 +1,9 @@
+def leaders(a,a_size):
+    currentmax = a[a_size-1]
+    print(currentmax)
+    for i in range(a_size-2,-1,-1):
+        if currentmax < a[i]:
+            print(a[i])
+            currentmax = a[i]
+a = [1,2,5433,12,405,13]          
+leaders(a,len(a))  
